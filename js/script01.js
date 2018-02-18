c917212333295b1090acedcd91a0707ecbad44d9
kalnynsh <kda869@yandex.ru>
2018-02-18 18:26:46 +0300

    Implement replacement '->", but worng doing don't -> don"t.

--- js/script01.js
+++ js/script01.js
@@ -1,2 +1,8 @@
 "use strict";
 
+var sourceText = document.querySelector(".content .content__info1").innerHTML;
+
+var reg = /(')(?=[\wа-яё]+?|\s*?)/gmi;
+var outText = sourceText.replace(reg, '"');
+
+document.querySelector(".content .content__info3").innerHTML = outText;
