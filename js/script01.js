c4f4a05dca25b22ca3fd6a2e4eb9eb1881e9a87f
kalnynsh <kda869@yandex.ru>
2018-02-19 13:36:27 +0300

    Create 2nd version for regular exp with group capturing.

--- js/script01.js
+++ js/script01.js
@@ -1,8 +1,12 @@
 "use strict";
 
 var sourceText = document.querySelector(".content .content__info1").innerHTML;
+// Version 1 with lookahead
+// var reg = /(')(?=[\wа-яё]+?|\s*?)/gim;
+// var outText = sourceText.replace(reg, '"');
 
-var reg = /(')(?=[\wа-яё]+?|\s*?)/gmi;
-var outText = sourceText.replace(reg, '"');
+// Version 2 with captured group
+var reg = /'([^']+(?:'[a-z][^']*?)*)'/gi;
+var outText = sourceText.replace(reg, '"$1"');
 
 document.querySelector(".content .content__info3").innerHTML = outText;
